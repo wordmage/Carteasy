3eb3a4dea952efd5a0902dc123e03317e941d372
Tosin <tosin@irokotv.com>
2015-10-30 19:07:39 +0000

    committed changes

--- carteasy/src/main/java/com/example/tech6/carteasy/SaveData.java
+++ carteasy/src/main/java/com/example/tech6/carteasy/SaveData.java
@@ -16,22 +16,28 @@ import java.io.IOException;
 
 /**
  * Created by tech6 on 10/28/15.
- *
- * Save function checkes if file exist and then save data to file in current apps directory
- *
- * UpdateValue function iterates through the existing data and updates a value based on its key and id
- *
- * checkIfExist checks if value exist before inserting, if exist do not save data.
  */
 public class SaveData {
 
-    public String save(Context context, JSONObject items, String id){
+    private Object updateReturnValue;
+
+
+    /*
+       Save function checkes if file exist, recieves data from User
+       Appends it and saves into users default app data storage space.
+    */
+    public String save(Context context, JSONObject items, String mid){
 
         String returnvalue = "data not saved";
 
+        /*
+           This retrieves the file from default application data storage if it exist.
+           If it doesn't It creates a new file.
+        */
         ContextWrapper cw = new ContextWrapper(context);
         File directory = cw.getDir("carteasy", Context.MODE_PRIVATE);
-        // Create imageDir
+
+        // Create imageDir in applications default directory
         File mypath = new File(directory, "test.json");
 
         if(mypath.exists()){
@@ -39,9 +45,10 @@ public class SaveData {
             JSONParser parser = new JSONParser();
             try {
 
+                // Get the JSON Object and loop through it.
                 Object obj = parser.parse(new FileReader(mypath));
                 JSONObject jsonObj = (JSONObject) obj;
-                if(!checkIfExist(id, jsonObj)) {
+                if(!checkIfIdExist(mid, jsonObj)) {
                     for (Object key : jsonObj.keySet()) {
                         //based on you key types
                         String keyStr = (String) key;
@@ -54,7 +61,9 @@ public class SaveData {
                         }
                     }
 
-                    //Push to file
+                    /*
+                      This writes to file in the user's default app data directory
+                    */
                     FileWriter filez = new FileWriter(mypath);
                     filez.write(items.toJSONString());
                     filez.flush();
@@ -64,7 +73,7 @@ public class SaveData {
 
                 } else {
 
-                    returnvalue = id+" exist";
+                    returnvalue = mid+" exist";
                 }
 
 
@@ -82,6 +91,10 @@ public class SaveData {
 
             try {
 
+                /*
+                   This assumes the file does not exist.
+                   So It creates a new file.
+                */
                 FileWriter filez = new FileWriter(mypath);
                 filez.write(items.toJSONString());
                 filez.flush();
@@ -96,25 +109,77 @@ public class SaveData {
     }
 
 
-    public void updateValue(String mid, String mkey, String newvalue, JSONObject jsonObj){
+    /*
+        UpdateValue function iterates through the existing data and updates a value based on its key and id
+        -> IN PROGRESS
+    */
+    public Object updateValue(String mid, String mkey, Object newvalue, Context context){
+        
 
-
+        return newvalue;
     }
 
-    public Boolean checkIfExist(String id, JSONObject jsonObj) {
+
+
+     /*
+        checkIfKeyExist function checks if a key exist already, so as not add multiple keys in same ID
+        For example: adding productname twice for id => 1234
+     */
+    public Boolean checkIfKeyExist(String mid, String mkey, JSONObject jsonObj) {
 
         Boolean exist = false;
+        Boolean found = false;
+
                 for (Object key : jsonObj.keySet()) {
                     //based on you key types
                     String keyStr = (String) key;
                     Object keyvalue = jsonObj.get(keyStr);
 
+
+                    if (keyStr.equals(mid)) {
+                        found = true;
+                    }
+
                     if (keyvalue instanceof JSONObject) {
-                        if (keyvalue.equals(id)) {
-                            exist = true;
+                        if(found.equals(true)) {
+                            JSONObject newJsonObj = (JSONObject) keyvalue;
+                            for (Object key2 : newJsonObj.keySet()) {
+                                //based on you key types
+                                String keyStr2 = (String) key2;
+                                Object keyvalue2 = newJsonObj.get(keyStr2);
+
+                                //return value
+                                if (keyStr2.equals(mkey)) {
+                                    //System.out.println("keyStr: " + keyStr2 + " keyValue: " + keyvalue2);
+                                    exist = true;
+                                }
+
+                            }
+                            found = false;
                         }
                     }
                 }
+        return exist;
+    }
+
+
+    /*
+       checkIfIdExist function checks if an ID exist, so as to prevent multiple Ids
+     */
+    public Boolean checkIfIdExist(String mid, JSONObject jsonObj) {
+
+        Boolean exist = false;
+
+        for (Object key : jsonObj.keySet()) {
+            //based on you key types
+            String keyStr = (String) key;
+            Object keyvalue = jsonObj.get(keyStr);
+
+
+            if (keyStr.equals(mid)) {
+                exist = true;
+            }
+        }
         return exist;
     }
 
