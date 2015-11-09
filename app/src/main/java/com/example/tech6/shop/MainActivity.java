8802a600fb80b9808632dd9ec876dd441b7dac69
Tosin <tosin@irokotv.com>
2015-11-09 11:25:48 +0100

    added new changes to mainactvity

--- app/src/main/java/com/example/tech6/shop/MainActivity.java
+++ app/src/main/java/com/example/tech6/shop/MainActivity.java
@@ -1,154 +1,48 @@
 package com.example.tech6.shop;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.example.tech6.carteasy.Carteasy;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import android.widget.GridView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
-    EditText prodid;
-    EditText productname;
-    EditText proddesc;
-    EditText prodq;
-    EditText isbn;
-    EditText shipping;
-    Button addtocart;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
 
-        prodid = (EditText) findViewById(R.id.productid);
-        productname = (EditText) findViewById(R.id.productname);
-        proddesc = (EditText) findViewById(R.id.proddesc);
-        prodq = (EditText) findViewById(R.id.prodq);
-        isbn = (EditText) findViewById(R.id.isbn);
-        shipping = (EditText) findViewById(R.id.shipping);
-        addtocart = (Button) findViewById(R.id.addtocart);
+        // Calling the RecyclerView
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
 
+        // The number of Columns
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-        addtocart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(prodid.getText().toString().matches("")){
-                    Toast.makeText(MainActivity.this, "Please enter product id", Toast.LENGTH_LONG).show();
-                }
-                else if(productname.getText().toString().matches("")){
-                    Toast.makeText(MainActivity.this, "Please enter product name", Toast.LENGTH_LONG).show();
-                }
-                else if(proddesc.getText().toString().matches("")){
-                    Toast.makeText(MainActivity.this, "Please enter product desc", Toast.LENGTH_LONG).show();
-                }
-                else if(prodq.getText().toString().matches("")){
-                    Toast.makeText(MainActivity.this, "Please enter quantity", Toast.LENGTH_LONG).show();
-                }
-                else if(isbn.getText().toString().matches("")){
-                    Toast.makeText(MainActivity.this, "Please enter isbn", Toast.LENGTH_LONG).show();
-                }
-                else if(shipping.getText().toString().matches("")){
-                    Toast.makeText(MainActivity.this, "Please enter shipping", Toast.LENGTH_LONG).show();
-                }else{
-                    //work
-                    Carteasy n = new Carteasy();
-                    n.add(prodid.getText().toString(),"productname", productname.getText().toString());
-                    n.add(prodid.getText().toString(),"proddesc", proddesc.getText().toString());
-                    n.add(prodid.getText().toString(),"prodq", prodq.getText().toString());
-                    n.add(prodid.getText().toString(), "isbn", isbn.getText().toString());
-                    n.add(prodid.getText().toString(), "shipping", shipping.getText().toString());
-                    n.commit(getApplicationContext());
-
-
-                    prodid.setText("");
-                    productname.setText("");
-                    proddesc.setText("");
-                    prodq.setText("");
-                    isbn.setText("");
-                    shipping.setText("");
-
-
-                }
-
-            }
-
-        });
-
-
-
-        //Carteasy n = new Carteasy();
-        //x11 is product id
-        /*n.add("x11", "name", "Barza");
-        n.add("x11", "quantity", "description of the product");
-        n.add("x11", "price", 97.51);
-        n.add("x11", "quantity", 7);
-        n.add("x11", "shipping", false);
-        n.add("x11", "isbn", 937883833L);
-
-        //x12 is product id
-        n.add("x12", "name", "Jumbo");
-        n.add("x12", "quantity", "description of the product");
-        n.add("x12", "price", 32.50);
-        n.add("x12", "quantity", 10);
-        n.add("x12", "shipping", true);
-        n.add("x12", "isbn", 2902882981L);
-
-        n.viewAll();*/
-
-        //Long isbn = (Long) n.get("x12", "isbn");
-
-        //Log.d("ISBN: ", isbn.toString());
-
-
-
-        /*JSONObject obj = new JSONObject();
-        obj.put("name", "mkyong.com");
-        obj.put("age", new Integer(100));
-
-        JSONArray list = new JSONArray();
-        JSONObject obj2 = new JSONObject();
-        JSONObject obj3 = new JSONObject();
-        obj3.put("msg 1","1");
-        obj3.put("msg 2","2");
-        obj3.put("msg 3","3");
-
-        JSONObject obj4 = new JSONObject();
-        obj4.put("msg 1","4");
-        obj4.put("msg 2", "5");
-        obj4.put("msg 3","6");
-
-
-        obj2.put("obj3",obj3);
-        obj2.put("obj4",obj4);
-
-
-
-        list.add(obj2);
-
-        obj.put("messages", list);
-
-        System.out.println("keyman: "+ obj);*/
-
-
+        mAdapter = new GridAdapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
