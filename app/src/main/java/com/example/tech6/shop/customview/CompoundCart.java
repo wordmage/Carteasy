7365da21d9eb51085328dd03f52881fea58c1b11
Tosin <tosin@irokotv.com>
2016-04-11 13:01:54 +0100

    compound views

---
+++ app/src/main/java/com/example/tech6/shop/customview/CompoundCart.java
@@ -0,0 +1,79 @@
+package com.example.tech6.shop.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.tech6.shop.R;
+
+/**
+ * Created by tech6 on 3/17/16.
+ */
+public class CompoundCart extends LinearLayout {
+
+    private TextView productName;
+    private TextView productDesc;
+    private TextView productPrice;
+    private TextView otherDetails;
+    private ImageView productImage;
+
+    public CompoundCart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setOrientation(LinearLayout.HORIZONTAL);
+        setGravity(Gravity.CENTER_VERTICAL);
+
+        TypedArray typedArray;
+        typedArray = context.obtainStyledAttributes(attrs, R.styleable.Options);
+
+        int descriptionColor = typedArray.getColor(R.styleable.Options_descriptionColor, Color.BLACK);
+        int textColor = typedArray.getColor(R.styleable.Options_textColor, Color.BLACK);
+
+        typedArray.recycle();
+
+        initializeViews(context);
+
+        productName = (TextView) this.findViewById(R.id.productname);
+        productDesc = (TextView) this.findViewById(R.id.productdesc);
+        otherDetails = (TextView) this.findViewById(R.id.otherdetails);
+        productPrice = (TextView) this.findViewById(R.id.productprice);
+        productImage = (ImageView) this.findViewById(R.id.productimage);
+
+    }
+
+    public void initializeViews(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.compoundcart, this);
+    }
+
+    public void setProductNameColor(int color) {
+        productName.setTextColor(color);
+    }
+
+    public void setProductDescColor(int color) {
+        productDesc.setTextColor(color);
+    }
+
+    public void setProductPriceColor(int color) {
+        productPrice.setTextColor(color);
+    }
+
+    public void setOtherDetailsColor(int color) {
+        otherDetails.setTextColor(color);
+    }
+
+    public void setProductImage(Bitmap bp) {
+        productImage.setImageBitmap(bp);
+    }
+
+
+
+
+}
